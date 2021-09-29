fbb81a70dcdf7201865a8fa85c83c3afc7ea5b11
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-09-29 17:32:24 +0100

    Add event listener to checkbox

--- src/check.js
+++ src/check.js
@@ -1,23 +1,10 @@
-/* eslint-disable import/prefer-default-export */
-// export default for (let i = 0; i < checkBoxes.length; i += 1) {
-//   checkBoxes[i].addEventListener('change', (e) => {
-//     const taskIndex = e.target.getAttribute('data-id');
-//     alert(taskIndex);
-//     if (checkBoxes[i].checked) {
-//       labelList[i].style.textDecoration = 'line-through';
-//     } else {
-//       labelList[i].style.textDecoration = 'none';
-//     }
-//   });
-// }
-
-export function displayBox() {
+function displayBox() {
   const labelList = document.querySelectorAll('.list');
   const checkBoxes = document.querySelectorAll('.check-list');
   for (let i = 0; i < checkBoxes.length; i += 1) {
-    checkBoxes[i].addEventListener('change', (e) => {
-      const taskIndex = e.target.getAttribute('data-id');
-      alert(taskIndex);
+    checkBoxes[i].addEventListener('change', () => {
+      // const taskIndex = e.target.getAttribute('data-id');
+      // alert(taskIndex);
       if (checkBoxes[i].checked) {
         labelList[i].style.textDecoration = 'line-through';
       } else {
@@ -27,3 +14,5 @@ export function displayBox() {
   }
 }
 displayBox();
+// eslint-disable-next-line import/prefer-default-export
+export { displayBox };
--- src/index.js
+++ src/index.js
@@ -29,32 +29,14 @@ const showTasks = (tasks) => `
 `;
 allTasks.innerHTML = listArray.map((tasks) => showTasks(tasks)).join('');
 
-// Checkbox
-// const labelList = document.querySelectorAll('.list');
-// const checkBoxes = document.querySelectorAll('.check-list');
-
-// function displayBox() {
-//   const labelList = document.querySelectorAll('.list');
-//   const checkBoxes = document.querySelectorAll('.check-list');
-//   for (let i = 0; i < checkBoxes.length; i += 1) {
-//     checkBoxes[i].addEventListener('change', (e) => {
-//       const taskIndex = e.target.getAttribute('data-id');
-//       alert(taskIndex);
-//       if (checkBoxes[i].checked) {
-//         labelList[i].style.textDecoration = 'line-through';
-//       } else {
-//         labelList[i].style.textDecoration = 'none';
-//       }
-//     });
-//   }
-// }
-// displayBox();
+displayBox();
 
 // local Storage
+
 const listArraySerialised = JSON.stringify(listArray);
 
 localStorage.setItem('listArray', listArraySerialised);
 
 const listArrayDeserialised = JSON.parse(localStorage.getItem('listArray'));
 
-console.log(listArrayDeserialised);
+listArrayDeserialised();
